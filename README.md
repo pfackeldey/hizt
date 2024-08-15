604889a756cdc01519eac6d7f9fb3569198802f2
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2024-08-15 17:37:15 +0200

    init project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hizt
 packed (compressed) boost-histograms/hists
+
+
+## Installation
+
+```bash
+git clone https://github.com/pfackeldey/hizt.git
+
+pip install -e .[hdf5,zarr,blosc2]
+```
---
+++ src/hizt/__init__.py
@@ -0,0 +1,21 @@
+"""
+hizt: packed (compressed) histograms.
+"""
+
+from __future__ import annotations
+
+__author__ = "Benjamin Fischer, Peter Fackeldey"
+__copyright__ = "Copyright 2024, Benjamin Fischer & Peter Fackeldey"
+__contact__ = "https://github.com/pfackeldey/hizt"
+__license__ = "BSD-3-Clause"
+__status__ = "Development"
+__version__ = "0.0.1"
+
+
+__all__ = [
+    "__version__",
+]
+
+
+def __dir__():
+    return __all__
