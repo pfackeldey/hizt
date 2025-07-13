02b17c622f2e72d449a5a278c3d6c2d7de9b2550
pfackeldey <fackeldey.peter@gmail.com>
2025-07-13 14:39:46 -0700

    update chunking

--- src/hizt/util.py
+++ src/hizt/util.py
@@ -51,9 +51,8 @@ _categorical_axes = (
 
 
 def _get_chunks(ax):
-    # this chunks categorical axes with 1 chunk, all others with 1000
-    # the categorical one is intuitive, the other one is arbitrarily chosen.
-    return 1 if isinstance(ax, _categorical_axes) else 1000
+    # this chunks categorical axes with 1 chunk, all others with None
+    return 1 if isinstance(ax, _categorical_axes) else None
 
 
 def _get_slice(refaxes: hist.axis.NamedAxesTuple, hist: hist.Hist) -> tuple[int, ...]:
